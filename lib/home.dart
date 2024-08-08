1a8805aeb8ff20a39292a9805f026f05e8e872e8
skooi928 <skooi866@gmail.com>
2024-08-08 18:46:23 +0800

    Add top appbar

--- lib/home.dart
+++ lib/home.dart
@@ -8,6 +8,16 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false, // This removes the back button
+        // Bottom border
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(1.0),
+          child: Container(
+            color: const Color(0xFF757575),
+            height: 1.0,
+          ),
+        ),
+        // Padding (top and bottom)
+        toolbarHeight: 65.0,
         // Left side
         // Circle Button with Avatar icon (clickable)
         leading: IconButton(
@@ -21,13 +31,50 @@ class HomePage extends StatelessWidget {
         ),
         // Center
         // Search bar with placeholder text
-        title: const TextField(
-          decoration: InputDecoration(
-            // Search icon
-            prefixIcon: Icon(Icons.search),
-            hintText: 'Search Doctor, Medicine, Tests...',
-            border: InputBorder.none,
-          ),
+        title: Row(
+          children: [
+            Expanded(
+              child: Container(
+                margin: const EdgeInsets.symmetric(vertical: 15.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: const Color(0xFFFFFFFF),
+                    isDense: true,
+                    // Padding
+                    contentPadding:
+                        const EdgeInsets.symmetric(horizontal: 15.0),
+                    // Search icon
+                    prefixIcon: const Icon(
+                      Icons.search,
+                      size: 22,
+                      color: Color(0xFF757575),
+                    ),
+                    hintText: 'Search Doctor, Medicine, Tests...',
+                    hintStyle: const TextStyle(
+                      fontSize: 16,
+                      color: Color(0xFF757575),
+                    ),
+                    border: OutlineInputBorder(
+                      borderSide:
+                          const BorderSide(color: Color(0xFF757575), width: 1),
+                      borderRadius: BorderRadius.circular(45),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide:
+                          const BorderSide(color: Color(0xFF757575), width: 1),
+                      borderRadius: BorderRadius.circular(45),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide:
+                          const BorderSide(color: Color(0xFF757575), width: 1),
+                      borderRadius: BorderRadius.circular(45),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
       body: const Center(
